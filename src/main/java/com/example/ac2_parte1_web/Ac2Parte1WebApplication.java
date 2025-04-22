fd5bc6bc2de5dfcdacc825ac6998645911149451
Leandro <leandro.lanzoni0400@gmail.com>
2025-04-22 19:36:58 -0300

    inicio

---
+++ src/main/java/com/example/ac2_parte1_web/Ac2Parte1WebApplication.java
@@ -0,0 +1,13 @@
+package com.example.ac2_parte1_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ac2Parte1WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ac2Parte1WebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ac2_parte1_web/Ac2Parte1WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ac2_parte1_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ac2Parte1WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
